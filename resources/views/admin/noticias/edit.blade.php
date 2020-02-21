9a2f4f455f9958083ed24b977653a53c5881a7d5
Luz <ulsa@6.local>
2020-02-21 12:47:38 -0700

    Ya muestra la imagen en el editar si es que contiene

--- resources/views/admin/noticias/edit.blade.php
+++ resources/views/admin/noticias/edit.blade.php
@@ -54,6 +54,22 @@
                             <input name= "txtTitulo" type="text" value="{{$noticia->titulo}}" class="form-control" />
     
                         </div>
+
+                        <div class="form-group" >
+                            <label for="">Imagen de Portada</label>
+                            <input type="file" name="imgPortada" class="form-control" />
+                        
+                        </div>
+
+                        @if($noticia->portada)
+
+                            <img style="width: 50px; heigh: auto;" src="/storage/portadas/{{$noticia->portada}}" />
+                        @else
+                        
+                            <p>No hay imagen cargada</p>
+
+                        @endif
+
                         <div class="form-group">
 
                             <label >Cuerpo</label>
